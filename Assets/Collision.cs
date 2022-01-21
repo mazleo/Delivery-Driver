08d23bf61b292779ff2e17c6e20edda10c32f3be
Jonnelin Leonardo <jonnelin.leonardo@gmail.com>
2022-01-20 22:36:04 -0500

    Complete project

--- Assets/Collision.cs
+++ Assets/Collision.cs
@@ -4,11 +4,23 @@ using UnityEngine;
 
 public class Collision : MonoBehaviour
 {
+    bool hasPackage = false;
+
     private void OnCollisionEnter2D(Collision2D other) {
         Debug.Log("I Just bumped into something!");
     }
 
     private void OnTriggerEnter2D(Collider2D other) {
-        Debug.Log("I triggered something!");
+        if (other.tag == "Package" && !hasPackage) {
+            Destroy(other.gameObject);
+            Debug.Log("Packaged picked up.");
+            hasPackage = true;
+        }
+        else if (other.tag == "Customer") {
+            if (hasPackage) {
+                Debug.Log("Delivered!");
+                hasPackage = false;
+            }
+        }
     }
 }
